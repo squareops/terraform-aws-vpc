b01499bc33dbf1a9f8ae34b513f98cc6b1da59b9
RohitSquareops <rohit.kumar@squareops.com>
2023-05-29 12:04:13 +0530

    updated version of vpc

--- examples/complete-vpc-with-vpn/main.tf
+++ examples/complete-vpc-with-vpn/main.tf
@@ -18,7 +18,7 @@ module "key_pair_vpn" {
 }
 
 module "vpc" {
-  source                                          = "squareops/vpc/aws"
+  source                                          = "../../"
   name                                            = local.name
   vpc_cidr                                        = local.vpc_cidr
   environment                                     = local.environment
