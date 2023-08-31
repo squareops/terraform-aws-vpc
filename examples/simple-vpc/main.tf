d632274b4b578cb2688687c8a1c6e7c9853419d3
Ajay-sops <ajay@squareops.com>
2023-08-31 12:29:17 +0530

    updated variable refrences in readme.md file

--- examples/simple-vpc/main.tf
+++ examples/simple-vpc/main.tf
@@ -11,7 +11,7 @@ locals {
 }
 
 module "vpc" {
-  source                = "../.."
+  source                = "squareops/vpc/aws"
   name                  = local.name
   vpc_cidr              = local.vpc_cidr
   environment           = local.environment
