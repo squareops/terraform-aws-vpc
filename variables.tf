0db6178653cf4669cc4e4be049ee38621fe1f434
rachit89 <115970922+rachit89@users.noreply.github.com>
2024-04-05 14:16:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,7 @@ variable "additional_aws_tags" {
   description = "Additional tags to be applied to AWS resources"
   type        = map(string)
   default     = {}
+}
 
 variable "additional_tags" {
   description = "Additional tags to be applied to AWS resources"
