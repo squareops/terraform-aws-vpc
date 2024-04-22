19d358ebc6af18609141ae6cde5d0edc7cfaf470
rachit89 <115970922+rachit89@users.noreply.github.com>
2024-04-22 17:39:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ module "vpc" {
   version                                         = "5.2.0"
   name                                            = format("%s-%s-vpc", var.environment, var.name)
   cidr                                            = var.vpc_cidr # CIDR FOR VPC
-  azs                                             = var.availability_zones
+  azs                                             = var.vpc_availability_zones
   use_ipam_pool                                   = var.ipam_enabled ? true : false
   ipv4_ipam_pool_id                               = var.ipam_enabled && var.ipam_pool_enabled ? aws_vpc_ipam_pool.ipam_pool[0].id : null
   ipv4_netmask_length                             = var.ipam_enabled ? var.ipv4_netmask_length : null
